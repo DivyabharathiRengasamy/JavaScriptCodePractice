9d9f056eaf33a86fedb446888c854022ef19f1ee
Divyabharathi <139893595+DivyabharathiRengasamy@users.noreply.github.com>
2023-09-08 20:44:57 +0530

    Add files via upload

---
+++ First.js
@@ -0,0 +1,10 @@
+// creating function 
+var printTableInReverseOrder=new function(){
+    for(let i=10;i<=10;i--)
+    {
+        console.log("2 x "+i+" = "+i*2);
+        if(i==1){
+            break;
+        }
+    }
+}
