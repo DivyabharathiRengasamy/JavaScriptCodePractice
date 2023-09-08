d1e403d9e9992250c745a7d2ede83a48e27e802e
Divyabharathi <139893595+DivyabharathiRengasamy@users.noreply.github.com>
2023-09-08 21:37:07 +0530

    Add files via upload

---
+++ Array.js
@@ -0,0 +1,16 @@
+//creating array by using literal
+var array1=[1,2,3,4,5];
+for(let i=0;i<array1.length;i++){
+    console.log(array1[i]);
+}
+//creating array by using new keyword
+var array2=new Array();
+array2[0]="Divya";
+array2[1]="Nithya";
+array2[2]="Agathiya";
+array2[3]="Surya";
+console.log(array2);
+//creating array by using constructor
+var array2=new Array(1000,2000,3000,4000);
+console.log(array2);
+
